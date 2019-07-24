768445bd301691074753264dbbd364db4914ddd7
saroj <saroj@mayvendev.com>
2019-07-24 14:17:13 +0545

    grid revised

--- webpack.mix.js
+++ webpack.mix.js
@@ -7,6 +7,6 @@ mix.setPublicPath('assets/compiled/');
 mix.options({
     processCssUrls: false
   })
-  .sass('assets/sass/theme.scss', 'css/theme.css')
+  .sass('assets/sass/app.scss', 'css/app.css')
   .js('assets/js/app.js', 'js/app.js')
   .version();
