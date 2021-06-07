50e48f1a6dd33ae7d13752b5f467bf4efd11d971
saroj <saroj@mayvendev.com>
2021-06-07 19:56:24 +0545

    webpack sass updated

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,7 @@
+const { browserSync } = require('laravel-mix');
 let mix = require('laravel-mix');
 require('laravel-mix-purgecss');
+require('browser-sync');
 
 mix.disableSuccessNotifications();
 mix.setPublicPath('assets/compiled/');
@@ -7,6 +9,8 @@ mix.setPublicPath('assets/compiled/');
 mix.options({
     processCssUrls: false
   })
+  .sourceMaps(false, 'source-map')
   .sass('assets/sass/app.scss', 'css/app.css')
   .js('assets/js/app.js', 'js/app.js')
+  // .browserSync('http://saroj-markup.test')
   .version();
